62cdcb378ded677bee7d5a4ea6aed987ab51f763
theCakeSphere <add9414@gmail.com>
2016-12-15 22:13:06 +0100

    Add files via upload

--- blatt08/version3/my_alloc.c
+++ blatt08/version3/my_alloc.c
@@ -1,196 +1,149 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "my_alloc.h"
 #include "my_system.h"
-#include <unistd.h>
+#include <stdio.h>
 
 #define BLOCKSIZE 8192
+#define MINBLOCKSIZE 8
 
 typedef struct memnode {
 	size_t size;
 	struct memnode* next;
 } memnode;
 
-memnode *root;
 memnode *node;
 
-//int totalblockcount=1;
+//prints the complete list of all free elements
+void printList(){
+	memnode *temp = node;
+	printf("list of free elements: ");
+	while(temp!=NULL){
+		//long ptrval = (long)temp%10000;
+		int size = temp->size;
+		//printf("%ld,%d --- ",ptrval,size);
+		printf("%d - ",size);
+		temp=temp->next;
+	}
+	printf("\n");
+}
 
+//returns the directly next element, it may be used or free
 memnode* successor(memnode* p) {
 	return (memnode*) ((char*)(p+1) + p->size);
 }
 
 void init_my_alloc() {
-	root = (memnode *)get_block_from_system();
-	//printf("root: \t\t%lu\n",(long)root);
-	//printf("last+1: \t%lu\n",(long)((char *)root+BLOCKSIZE));
-	root->size = 0;
-	root->next = successor(root);
-	root->next->size = BLOCKSIZE-2*sizeof(memnode);
-	root->next->next = NULL;
-	//printf("root next: \t%lu\n",(long)root->next);
-	//printf("root next size: %lu\n",(long)root->next->size);
-	node = root->next;
-	//printf("node: \t\t%lu\n",(long)node);
-	//printf("node size: %lu\n",(long)node->size);
+	node = (memnode *)get_block_from_system();
+	node->size = BLOCKSIZE-2*sizeof(memnode);
+	node->next = NULL;
+	
+	//printf("node: \t\t%ld\n",(long)node);
+	//printf("node: \t\t%ld\n",(long)node%10000);
+	//printf("node->next: \t%ld\n",(long)node->next);
+	//printf("node->size: %ld\n",(long)node->size);
 	//printf("-------\n");
 }
 
 void* my_alloc(size_t size) {
-	//printf("alloc size : %lu\n",(long)size);
-	//printf("node: \t\t%lu\n",(long)node);
-	//printf("node size: %lu\n",(long)node->size);
-	//printf("node->next: \t%lu\n",(long)node->next);
-	memnode *temp=node;
-	memnode *prev=NULL;
+	//printf("alloc size : %ld\n",(long)size);
+	//printList();
 	
-	//printf("totalblockcount: %d\n",totalblockcount);
-	//memnode *test=node;
-	//printf("search start.. \n");
-	//while(test!=NULL){
-	//	test=test->next;
-	//}
-	//printf("search end\n");
+	memnode *prev = NULL;
+	memnode *temp = node;
 	
-	//search for free memory in list, starting with node
+	//searches for element with enough space or end of list, starting with node
 	while(temp!=NULL&&size>temp->size){
-		//printf("temp: \t\t%lu\n",(long)temp);
 		prev = temp;
 		temp=temp->next;
-		//printf("temp: \t\t%lu\n",(long)temp);
-		//if(temp!=NULL) printf("temp->size: \t%lu\n",(long)temp->size);
-		//usleep(20000);
 	}
-	//printf("prev: \t\t%lu\n",(long)prev);
-	//printf("temp: \t\t%lu\n",(long)temp);
-	//printf("temp->next: \t%lu\n",(long)(temp->next));
-	//printf("temp->size: \t%lu\n",(long)temp->size);
 	
-	//not enough free memory left
+	//printf("prev: \t\t%ld\n",(long)prev%10000);
+	//printf("temp: \t\t%ld\n",(long)temp%10000);
+	//printf("temp->size: %ld\n",(long)temp->size);
+	//printf("temp->next: \t%ld\n",(long)temp->next%10000);
+	
+	
+	//not enough free memory left, add new block at beginning of list
 	if(temp==NULL){
-		printf("temp = NULL, get new block from system\n");
-		memnode *retnode = (memnode *)get_block_from_system();
-		//totalblockcount++;
+		//printf("alloc size : %ld\n",(long)size);
+		//printf("not enough free memory left, getting new block\n");
+		//printList();
 		
-		retnode->next = NULL;
-		retnode->size = size;
-		memnode *new = successor(retnode);
-		new->size = BLOCKSIZE-2*sizeof(memnode)-size;
-		new->next = NULL;
-		
-		//prev != NULL, node should never be NULL
-		prev->next = new;
-		
-		char *ret = (char *)retnode + sizeof(memnode);
-		return ret;
+		memnode *newelement = (memnode *)get_block_from_system();
+		newelement->size=BLOCKSIZE-sizeof(memnode);
+		newelement->next = node;
+		node = newelement;
+		temp = node;
+		prev = NULL;
+		//printList();
 	}
 	
-	//if prev==NULL --> temp==node
-	
-	//printf("temp->size: %d\n",(int)temp->size);
-	//printf("size: %d\n",(int)(temp->size-sizeof(memnode)-8));
-	//enough free memory left for split
-	if((long)size<=(long)(temp->size-sizeof(memnode)-8)){
+	//enough free memory left for splitting one memory block into two
+	if((long)size<=(long)(temp->size-sizeof(memnode)- MINBLOCKSIZE)){
 		//printf("enough free memory left for split\n");
-		//printf("temp: \t\t%lu\n",(long)temp);
-		//printf("temp size: %lu\n",(long)temp->size);
-		memnode *retnode = temp;
 		
-		//temp = new element in right "half" of the free memory block
-		temp = (memnode *)((char *)temp+sizeof(memnode)+size);
-		temp->size = retnode->size - sizeof(memnode) - size;
-		temp->next = retnode->next;
+		//printf("temp: %p\n",temp);
+		//printf("temp: %ld\n",(long)temp%10000);
+		memnode *newelement = (memnode *)((char *)temp + sizeof(memnode) + size);
+		//printf("newnode: %ld\n",(long)newelement%10000);
 		
-		//printf("temp2: \t\t%lu\n",(long)temp);
-		//printf("temp2 size: %d\n",(int)temp->size);
+		newelement->next = temp->next;
+		newelement->size = temp->size - sizeof(memnode) - size;
+		
+		temp->size = size;
+		temp->next = NULL;
 		
 		if(prev!=NULL){
-			prev->next = temp;
-			//printf("prev->next: \t%lu\n",(long)prev->next);
+			prev->next = newelement;
+			
 		}else{
-			node = temp;
+			node = newelement;
+			
 		}
 		
-		retnode->size = size;
-		retnode->next = NULL;
-		
-		//printf("prev: \t\t%lu\n",(long)prev);
-		//printf("retnode: \t%lu\n",(long)retnode);
-		//printf("retnode->size: %lu\n",(long)retnode->size);
-		//printf("node: \t\t%lu\n",(long)node);
-		//printf("node->size: \t%lu\n",(long)node->size);
-		//printf("node->next: \t%lu\n",(long)node->next);
-		
-		char *ret = (char *)retnode + sizeof(memnode);
-		//printf("ret: \t\t%lu\n",(long)ret);
-		//printf("--------\n");
-		return ret;
-		
-		//exact fit
-	} else if(size == temp->size || size == temp->size-8 || size == temp->size-16){
+		//printList();
+		//printf("return ptr: %p\n",(temp+1));
+		//printf("return ptr: %ld\n",(long)(temp+1));
+		return (void *)(temp+1);
+	}
+	
+	//fits exactly or too small to split
+	else{
 		//printf("fits exactly\n");
-		memnode *retnode = temp;
-		//temp = get_block_from_system();
 		
-		if(temp->next!=NULL){
-			if(prev!=NULL){
-				prev->next = temp->next;
-			}else{
-				node = temp->next;
-			}
+		if(prev!=NULL){
+			prev->next = temp->next;
+			
 		}else{
-			if(prev!=NULL){
-				prev->next=NULL;
+			if(temp->next != NULL){
+				node = temp->next;
+				
 			}else{
-				//no prev, create new block
-				printf("prev = NULL,next = NULL, get new block from system\n");
 				node = (memnode *)get_block_from_system();
-				
-				//totalblockcount++;
-				
-				node->size = BLOCKSIZE;
+				node->size = BLOCKSIZE - sizeof(memnode);
 				node->next = NULL;
 			}
 		}
-		//printf("node: \t\t%lu\n",(long)node);
-		char *ret = (char *)retnode + sizeof(memnode);
-		return ret;
+		temp->next = NULL;
 		
-	}else{
-		//printf("does not fit exactly\n");
-		
-		//printf("temp: \t\t%lu\n",(long)temp);
-		//printf("temp->next: \t%lu\n",(long)temp->next);
-		//printf("temp->size: \t%lu\n",(long)temp->size);
-		
-		memnode *retnode = (memnode *)get_block_from_system();
-		retnode->next = NULL;
-		retnode->size = size;
-		memnode *new = successor(retnode);
-		new->size = BLOCKSIZE-2*sizeof(memnode)-size;
-		new->next = temp->next;
-		temp->next = new;
-		
-		char *ret = (char *)retnode + sizeof(memnode);
-		return ret;
-		
+		//printList();
+		//printf("return ptr: %ld\n",(long)(temp+1));
+		return (void *)(temp+1);
 	}
-	
-	printf("should never reach this\n");
-	return NULL;
 	
 }
 
 void my_free(void* ptr) {
-	//printf("free: \t\t%lu\n",(long)ptr);
+	//printf("free: \t\t%ld\n",(long)(ptr-1));
 	
+	//set free memnode at the beginning of the list
+	memnode *temp = node;
+	node = (memnode *)((char *)ptr-sizeof(memnode));
+	node->next = temp;
+	
+	/*
 	memnode *temp = node->next;
-	node->next = (memnode *)(ptr-sizeof(memnode));
+	node->next = (memnode *)((char *)ptr-sizeof(memnode));
 	node->next->next = temp;
-	
-	//printf("node: \t\t%lu\n",(long)node);
-	//printf("node->next: \t%lu\n",(long)node->next);
-	//printf("node->n->n: \t%lu\n",(long)node->next->next);
-	
-	//printf("---\n");
+	*/
 }
