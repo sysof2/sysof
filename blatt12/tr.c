dbe08d6f22a444f5d5eb4f61a8adfcb171355c2f
theCakeSphere <add9414@gmail.com>
2017-01-31 17:19:03 +0100

    Add files via upload

--- blatt12/tr.c
+++ blatt12/tr.c
@@ -4,53 +4,119 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, const char* argv[]){
-    const char* cmdname = *argv++; argc--;
-    
-    if(argc !=3){
-        printf("zu wenig Argumente! Es werden 3 benoetigt!\n");
-        exit(1);
-    }
-    
-    const char* reg1 = *argv++; argc--;
-    const char*reg2 = *argv++; argc--;
-    const char* file = *argv++; argc--;
-    printf("reg1: %s, reg2: %s, filename:%s\n", reg1, reg2, file);
-    
-    int fd;
-    if((fd=open(file,O_RDWR))<0){
-        perror("open");
-        exit(1);
-    }
-    
-    struct stat statbuf;
-    
-    if(fstat(fd,&statbuf)<0){
-        perror("fstat");
-        exit(1);
-    }
-    
-    off_t nbytes = statbuf.st_size;
-    
-    unsigned char* buf = (unsigned char*) mmap(0, nbytes, PROT_READ||PROT_WRITE, MAP_SHARED, fd, 0);
-    if(buf == MAP_FAILED) {
-        printf("mmap failed!!\n");
-    }
-    
-    for(unsigned char* cp=buf; cp<buf+nbytes; ++cp) {
-        //hier findet dann das translate statt - fehlt noch...
-        unsigned char ch = *cp;
-        const char* temp1 = reg1;
-        const char* temp2 = reg2;
-        
-        for
-        
-        printf("%c",ch);
-    }
-    
-    munmap(buf,nbytes);
-    close(fd);
-    
-    return 0;
-}
+	//const char* cmdname = *argv++; argc--;
+	argv++;
+	
+	if(--argc !=3){
+		printf("zu wenig Argumente! Es werden 3 benoetigt!\n");
+		exit(1);
+	}
+	
+	const char* reg1 = *argv++; argc--;
+	const char* reg2 = *argv++; argc--;
+	const char* file = *argv++; argc--;
+	printf("reg1: %s, reg2: %s, filename:%s\n", reg1, reg2, file);
+	
+	char srclist[256];
+	char destlist[256];
+	
+	if(reg1[0]=='-'||reg1[strlen(reg1)-1]=='-'){
+		printf("incorrect use of '-'\n");
+		exit(1);
+	}
+	
+	//expand '-' syntax to full vector
+	
+	int pos=0;
+	for(int i=0;i<strlen(reg1);i++){
+		if(reg1[i]=='-'){
+			if(reg1[i-1]>=reg1[i+1]){
+				printf("incorrect use of '-'\n");
+				exit(1);
+			}
+			
+			for(char ch=reg1[i-1]+1;ch<reg1[i+1];ch++){
+				srclist[pos]=ch;
+				pos++;
+			}
+			
+		}else{
+			srclist[pos]=reg1[i];
+			pos++;
+		}
+	}
+	srclist[pos]=0;
+	
+	pos=0;
+	for(int i=0;i<strlen(reg2);i++){
+		if(reg2[i]=='-'){
+			if(reg2[i-1]>=reg2[i+1]){
+				printf("incorrect use of '-'\n");
+				exit(1);
+			}
+			
+			for(char ch=reg2[i-1]+1;ch<reg2[i+1];ch++){
+				destlist[pos]=ch;
+				pos++;
+			}
+			
+		}else{
+			destlist[pos]=reg2[i];
+			pos++;
+		}
+	}
+	destlist[pos]=0;
+	
+	if(strlen(srclist)!=strlen(destlist)){
+		printf("arguments must be the same length\n");
+	}
+	
+	//printf("%s\n",srclist);
+	//printf("%s\n",destlist);
+	
+	
+	int fd = open(file,O_RDWR);
+	if(fd<0){
+		perror("open");
+		exit(1);
+	}
+	
+	struct stat statbuf;
+	
+	if(fstat(fd,&statbuf)<0){
+		perror("fstat");
+		exit(1);
+	}
+	
+	off_t nbytes = statbuf.st_size;
+	
+	unsigned char* buf = (unsigned char*) mmap(0, nbytes, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	if(buf == MAP_FAILED) {
+		printf("mmap failed!!\n");
+	}
+	
+	for(unsigned char* cp=buf; cp<buf+nbytes; ++cp) {
+		unsigned char ch = *cp;
+		
+		for(int i=0;i<strlen(srclist);i++){
+			if(srclist[i]==ch){
+				ch=destlist[i];
+			}
+		}
+		
+		//Change the file:
+		*cp = ch;
+		
+		//printf("%c",*cp);
+	}
+	
+	munmap(buf,nbytes);
+	close(fd);
+	
+	printf("success\n");
+	
+	return 0;
+} 
